e589e4e19a5823746272e63e4372bf18927b6a41
Jahid Hasan (Morshed) <jh3495689@gmail.com>
2026-02-21 01:14:12 +0600

    add banner title with responsive

--- src/app/(withCommonLayout)/page.tsx
+++ src/app/(withCommonLayout)/page.tsx
@@ -1,9 +1,10 @@
+import Banner from "@/src/components/home/banner/Banner";
 import React from "react";
 
 const page = () => {
   return (
-    <div>
-      <h1>this is home page with common layout</h1>
+    <div className="xs:mt-8 xs:mx-4 xl:mt-8 xl:mx-15">
+      <Banner />
     </div>
   );
 };
---
+++ src/components/home/banner/Banner.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div>
+      {/* banner title */}
+      <h1 className="font-rubik xs:text-[60px] md:text-[120px] xl:text-[223.5px] leading-none tracking-normal uppercase font-bold text-dark-gray my-6">
+        DO IT <span className="text-blue">RIGHT</span>
+      </h1>
+
+      {/* banner image */}
+    </div>
+  );
+};
+
+export default Banner;
